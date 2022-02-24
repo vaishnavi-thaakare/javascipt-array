60652e1149f956c41e91f3b824cf6440fa3193cf
vaishnavithakare2807 <vaishnavithaakare@gmail.com>
2022-02-24 18:28:03 +0530

    add nesting array and object inside object

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
-const number = [1, 15, 89, 45, 52];
-console.log(number[1]);
-number[1] = 51;
-console.log(number)
+var object1 = [{
+    
+        userName: "vaishnavi",
+        gender: "female",
+        age: 25
+    } ,{
+        userName: "neha",
+        gender: "female",
+        age: 20
+    } ,{
+        userName: "pratiksha",
+        gender: "female",
+        age: 18
+    }]
+
+console.log(object1)
+
+
+
+// Array nested inside an object1
+
+var object2 = {
+    firstName: "vaishnavi",
+    value: [1, 5, 6, 6, 6],
+    time:[1547,4596,1897,5865]
+}
+console.log(object2)
