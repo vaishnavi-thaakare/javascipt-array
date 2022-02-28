2c0491d013656bd8044e51c366f4c811942e8501
vaishnavithakare2807 <vaishnavithaakare@gmail.com>
2022-02-28 09:51:21 +0530

    add

--- app.js
+++ app.js
@@ -1,27 +1,2 @@
-var object1 = [{
-    
-        userName: "vaishnavi",
-        gender: "female",
-        age: 25
-    } ,{
-        userName: "neha",
-        gender: "female",
-        age: 20
-    } ,{
-        userName: "pratiksha",
-        gender: "female",
-        age: 18
-    }]
-
-console.log(object1)
-
-
-
-// Array nested inside an object1
-
-var object2 = {
-    firstName: "vaishnavi",
-    value: [1, 5, 6, 6, 6],
-    time:[1547,4596,1897,5865]
-}
-console.log(object2)
+const month = ["jan", "feb", "march", "may", "jun", "july"];
+console.log(month.splice(3,0,"april"))
